c14a4f715a62d2c1b5b5f8b20c750a0ce256130f
Skylinnnnnn <ltldev561@gmail.com>
2024-11-06 19:28:50 -0800

    fix: add back weixin domain and deploy

--- next.config.js
+++ next.config.js
@@ -15,6 +15,12 @@ const nextConfig = {
                 port: "",
                 pathname: "/storage/v1/object/public/**",
             },
+            {
+                protocol: "https",
+                hostname: "mp.weixin.qq.com",
+                port: "",
+                pathname: "/**",
+            },
         ],
     },
 };
