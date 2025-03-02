b235fcbb9d9c2be4d06a31b5b8fa7d772ee3d8fd
Skylinnnnnn <ltldev561@gmail.com>
2025-03-02 05:02:28 -0800

    chore: better build

--- app/api/user-dashboard-home-data/route.ts
+++ app/api/user-dashboard-home-data/route.ts
@@ -3,6 +3,8 @@ import { fetchFromTable } from "@/utils/actions/supabase";
 
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(req: Request) {
     try {
         const { searchParams } = new URL(req.url);
