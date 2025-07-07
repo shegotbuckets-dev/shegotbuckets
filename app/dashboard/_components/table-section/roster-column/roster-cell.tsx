4db8eb77671c755afc2fa139592f64f2d7fd76cd
Skylinnnnnn <ltldev561@gmail.com>
2025-07-06 20:29:57 -0700

    feat: teams drop down only show the team that is linked to the league

--- app/dashboard/_components/table-section/roster-column/roster-cell.tsx
+++ app/dashboard/_components/table-section/roster-column/roster-cell.tsx
@@ -9,17 +9,6 @@ import { Badge } from "@/components/ui/badge";
 import { RosterButton } from "./roster-button";
 
 export const RosterCell = ({ event }: BaseCellProps) => {
-    if (!event.active) {
-        return (
-            <Badge
-                variant={event.userStatus.isRegistered ? "green" : "secondary"}
-                className={STATUS_BADGE_CLASSNAME}
-            >
-                {event.userStatus.isRegistered ? BADGE_TEXT.YES : BADGE_TEXT.NA}
-            </Badge>
-        );
-    }
-
     if (event.userStatus.isRegistered) {
         return (
             <Badge variant="green" className={STATUS_BADGE_CLASSNAME_CLICKABLE}>
